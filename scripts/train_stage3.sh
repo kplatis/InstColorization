a8d254d029946eebfacaca8b9b5ca76430e792b1
Konstantinos Platis <kplatis@protonmail.com>
2021-07-19 22:30:02 +0200

    add printing of log

--- scripts/train_stage3.sh
+++ scripts/train_stage3.sh
@@ -5,4 +5,4 @@ mkdir ./checkpoints/coco_mask
 cp ./checkpoints/coco_full/latest_net_G.pth ./checkpoints/coco_mask/latest_net_GF.pth
 cp ./checkpoints/coco_instance/latest_net_G.pth ./checkpoints/coco_mask/latest_net_G.pth
 cp ./checkpoints/coco_full/latest_net_G.pth ./checkpoints/coco_mask/latest_net_GComp.pth
-python train.py --stage fusion --name coco_mask --sample_p 1.0 --niter 1 --niter_decay 1 --lr 0.00005 --model train --load_model --display_ncols 4 --fineSize 256 --batch_size 1 --display_freq 500 --print_freq 500 --train_img_dir $DATASET_DIR
+python train.py --stage fusion --name coco_mask --sample_p 1.0 --niter 1 --niter_decay 1 --lr 0.00005 --model train --load_model --display_ncols 4 --fineSize 256 --batch_size 1 --display_freq 50 --print_freq 50 --train_img_dir $DATASET_DIR --no_html
