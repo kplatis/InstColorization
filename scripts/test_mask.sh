3a1326d3dce26824d15cf9642df7197875523462
Konstantinos Platis <kplatis@protonmail.com>
2021-07-23 21:22:33 +0200

    Add validation

--- scripts/test_mask.sh
+++ scripts/test_mask.sh
@@ -2,4 +2,4 @@ INPUT_DIR=val_data/val_tiny
 OUTPUT_DIR=results
 
 python inference_bbox.py --test_img_dir $INPUT_DIR
-python test_fusion.py --name test_fusion --sample_p 1.0 --model fusion --fineSize 256 --test_img_dir $INPUT_DIR --results_img_dir $OUTPUT_DIR
+python validate_fusion.py --niter 1 --niter_decay 0 --name test_fusion --sample_p 1.0 --model fusion --fineSize 256 --test_img_dir $INPUT_DIR --results_img_dir $OUTPUT_DIR
