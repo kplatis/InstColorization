46fdb627ed9c9c83ef27aacf7e21aefaeb56db44
Konstantinos Platis <kplatis@protonmail.com>
2021-07-23 19:31:15 +0200

    Add csv storing

--- scripts/train_stage3.sh
+++ scripts/train_stage3.sh
@@ -5,4 +5,4 @@ mkdir ./checkpoints/coco_mask
 cp ./checkpoints/coco_full/latest_net_G.pth ./checkpoints/coco_mask/latest_net_GF.pth
 cp ./checkpoints/coco_instance/latest_net_G.pth ./checkpoints/coco_mask/latest_net_G.pth
 cp ./checkpoints/coco_full/latest_net_G.pth ./checkpoints/coco_mask/latest_net_GComp.pth
-python train.py --stage fusion --name coco_mask --sample_p 1.0 --niter 5 --niter_decay 0 --lr 0.00005 --model train --load_model --display_ncols 4 --fineSize 256 --batch_size 1 --display_freq 1 --print_freq 1 --train_img_dir $DATASET_DIR --no_html
+python train.py --stage fusion --name coco_mask --sample_p 1.0 --niter 1 --niter_decay 0 --lr 0.00005 --model train --load_model --display_ncols 4 --fineSize 256 --batch_size 1 --display_freq 1 --print_freq 1 --train_img_dir $DATASET_DIR --no_html
