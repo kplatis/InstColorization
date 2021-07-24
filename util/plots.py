05db0f502d64d681cd34ce1b295ff45ef3105240
Konstantinos Platis <kplatis@protonmail.com>
2021-07-24 12:26:47 +0200

    fix plots

--- util/plots.py
+++ util/plots.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 
 
 def plot_losses(train_list, validation_list, learning_rate, epochs):
-	epochs = range(1, len(train_list) + 1)
+	epoch_range = range(1, len(train_list) + 1)
 	# plotting the line 1 points
-	plt.plot(epochs, train_list, label="Train")
+	plt.plot(epoch_range, train_list, label="Train")
 	# plotting the line 2 points
-	plt.plot(epochs, validation_list, label="Validation")
+	plt.plot(epoch_range, validation_list, label="Validation")
 	plt.xlabel('Epochs')
 	# Set the y axis label of the current axis.
 	plt.ylabel('Losses')
